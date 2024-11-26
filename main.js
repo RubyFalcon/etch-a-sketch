8c58ec635a70901b8ef71a0d74db04cb7b3193fc
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-26 15:25:22 +0000

    create a function which returns the divs at height + width to have equal rows and columns

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const container = document.querySelector('.container');
+//todo: turn the for into n  * n and use n to get the width and height of the div
+// e.g div.height = container.height / n;
+let n = 16
 
-for (let i=0; i <(16*16); i++){
-    let squaredDiv = document.createElement('div');
-    squaredDiv.classList.add("square-div");
-    container.appendChild(squaredDiv);
+
+
+
+function createDivs(n){
+    let axis = (960/n)-16;
+    console.log(axis);
+
+    for (let i=0; i <(n**2); i++){
+        let squaredDiv = document.createElement('div');
+        squaredDiv.classList.add("square-div");
+        container.appendChild(squaredDiv);
+        squaredDiv.setAttribute("style", `width: ${axis}px; height: ${axis}px`)
+    }
     
 }
+
+createDivs(16);
