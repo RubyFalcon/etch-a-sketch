03ef99f4e1141b5faa343256ce22507852f93428
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-26 14:43:09 +0000

    create initial setup of web-page

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+for (let i=0; i <16; i++){
+    let squaredDiv = document.createElement('div');
+    squaredDiv.classList.add("square-div");
+    container.appendChild(squaredDiv);
+    
+}
