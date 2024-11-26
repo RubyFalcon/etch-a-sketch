37e40cfc7e94ffb4da9345928031969a26be9a91
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-11-26 14:51:27 +0000

    create a 16x16 grid of divs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 
-for (let i=0; i <16; i++){
+for (let i=0; i <(16*16); i++){
     let squaredDiv = document.createElement('div');
     squaredDiv.classList.add("square-div");
     container.appendChild(squaredDiv);
